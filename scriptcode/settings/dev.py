eaa87f60d2dd99fe9aa72e17026cddc3f1593373
McleanTheNerd <mcleanmartinchikadaya@gmail.com>
2022-07-19 07:58:51 +0100

    init

---
+++ scriptcode/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-p9!mv0e*us2i%1*%yzc&jd)@c04j3)@+0)172##50+7gjkbu0k"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
